4d265a3d37748225bf4765e35cd366eafcb12fcc
giancms <giancarlos.msosa@gmail.com>
2022-11-12 18:23:19 -0500

    Final edition

--- main.py
+++ main.py
@@ -1,119 +1,59 @@
-import pygame, sys, time, random
+import pygame
+import numpy as np
+import time
+import math
 
-speed = 15
+pygame.init()
+size = width, height = 700, 700
+numberXCells = 60
+numberYCells = 60
+sizeCellWidth = (width - 1) / numberXCells
+sizeCellHeight = (height - 1) / numberYCells
+background = 25, 25, 25
+screen = pygame.display.set_mode((height, width), pygame.RESIZABLE)
+screen.fill(background)
+gameState = np.zeros((numberXCells, numberYCells))
+print(gameState)
+pauseExec = False
+while 1:
+    newGameState = np.copy(gameState)
+    eventt = pygame.event.get()
+    for event in eventt:
+        if event.type == pygame.KEYDOWN:
+            pauseExec = not pauseExec
+        mouseClick = pygame.mouse.get_pressed()
+        if sum(mouseClick) > 0:
+            positionX, positionY = pygame.mouse.get_pos()
+            if 0 < positionX < (width - 1) and 0 < positionY < (height - 1):
+                newGameState[math.floor(positionX / sizeCellWidth),
+                             math.floor(positionY / sizeCellHeight)] = mouseClick[0] and not mouseClick[2]
+    screen.fill(background)
+    for y in range(0, numberYCells):
+        for x in range(0, numberXCells):
+            if not pauseExec:
+                numberNeigh = gameState[(x - 1) % numberXCells, (y - 1) % numberYCells] + \
+                              gameState[(x) % numberXCells, (y - 1) % numberYCells] + \
+                              gameState[(x + 1) % numberXCells, (y - 1) % numberYCells] + \
+                              gameState[(x - 1) % numberXCells, (y) % numberYCells] + \
+                              gameState[(x + 1) % numberXCells, (y) % numberYCells] + \
+                              gameState[(x - 1) % numberXCells, (y + 1) % numberYCells] + \
+                              gameState[(x) % numberXCells, (y + 1) % numberYCells] + \
+                              gameState[(x + 1) % numberXCells, (y + 1) % numberYCells]
+                # R1: Una celula muerta con exactamente 3 celulas vecinas vivas NACE
+                if gameState[x, y] == 0 and numberNeigh == 3:
+                    newGameState[x, y] = 1
+                # R2: Una celula viva con 2 o 3 celulas vecinas vivas sigue VIVA, en otro caso MUERE
+                elif gameState[x, y] == 1 and (numberNeigh < 2 or numberNeigh > 3):
+                    newGameState[x, y] = 0
 
-frame_size_x = 1380
-frame_size_y = 840
-
-check_errors = pygame.init()
-
-if check_errors[1] > 0:
-    print("Error " + check_errors[1])
-else:
-    print("Juego iniciado con éxito!")
-
-pygame.display.set_caption("Snake Game")
-game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
-
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
-
-fps_controller = pygame.time.Clock()
-
-square_size = 60
-
-
-def init_vars():
-    global head_pos, snake_body, food_pos, food_spawn, score, direction
-    direction = "RIGHT"
-    head_pos = [120, 60]
-    snake_body = [[120, 60]]
-    food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size,
-                random.randrange(1, (frame_size_y // square_size)) * square_size]
-    food_spawn = True
-    score = 0
-
-
-init_vars()
-
-
-def show_score(choice, color, font, size):
-    score_font = pygame.font.SysFont(font, size)
-    score_surface = score_font.render("Score: " + str(score), True, color)
-    score_rect = score_surface.get_rect()
-    if choice == 1:
-        score_rect.midtop = (frame_size_x / 10, 15)
-    else:
-        score_rect.midtop = (frame_size_x / 2, frame_size_y / 1.25)
-
-    game_window.blit(score_surface, score_rect)
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_UP or event.key == ord("w")
-                    and direction != "DOWN"):
-                direction = "UP"
-            elif (event.key == pygame.K_DOWN or event.key == ord("s")
-                  and direction != "UP"):
-                direction = "DOWN"
-            elif (event.key == pygame.K_LEFT or event.key == ord("a")
-                  and direction != "RIGHT"):
-                direction = "LEFT"
-            elif (event.key == pygame.K_RIGHT or event.key == ord("d")
-                  and direction != "LEFT"):
-                direction = "RIGHT"
-
-    if direction == "UP":
-        head_pos[1] -= square_size
-    elif direction == "DOWN":
-        head_pos[1] += square_size
-    elif direction == "LEFT":
-        head_pos[0] -= square_size
-    else:
-        head_pos[0] += square_size
-
-    if head_pos[0] < 0:
-        head_pos[0] = frame_size_x - square_size
-    elif head_pos[0] > frame_size_x - square_size:
-        head_pos[0] = 0
-    elif head_pos[1] < 0:
-        head_pos[1] = frame_size_y - square_size
-    elif head_pos[1] > frame_size_y - square_size:
-        head_pos[1] = 0
-
-    snake_body.insert(0, list(head_pos))
-    if head_pos[0] == food_pos[0] and head_pos[1] == food_pos[1]:
-        score += 1
-        food_spawn = False
-    else:
-        snake_body.pop()
-
-    if not food_spawn:
-        food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size,
-                    random.randrange(1, (frame_size_y // square_size)) * square_size]
-        food_spawn = True
-
-    game_window.fill(black)
-    for pos in snake_body:
-        pygame.draw.rect(game_window, green, pygame.Rect(
-            pos[0] + 2, pos[1] + 2,
-            square_size - 2, square_size - 2))
-
-    pygame.draw.rect(game_window, red, pygame.Rect(food_pos[0],
-                                                   food_pos[1], square_size, square_size))
-
-    for block in snake_body[1:]:
-        if head_pos[0] == block[0] and head_pos[1] == block[1]:
-            init_vars()
-
-    show_score(1, white, 'consolas', 20)
-    pygame.display.update()
-    fps_controller.tick(speed)
+            poly = [((x) * sizeCellWidth, (y) * sizeCellHeight),
+                    ((x + 1) * sizeCellWidth, (y) * sizeCellHeight),
+                    ((x + 1) * sizeCellWidth, (y + 1) * sizeCellHeight),
+                    (x * sizeCellWidth, (y + 1) * sizeCellHeight)]
+            if newGameState[x, y] == 0:
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+            else:
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 0)
+    time.sleep(1 / 30)
+    gameState = np.copy(newGameState)
+    pygame.display.flip()
